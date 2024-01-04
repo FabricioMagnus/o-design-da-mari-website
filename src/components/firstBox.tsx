19db04f9f0aa09b43cdc34ca9dee29516d5b3dc6
Fabricio Magnus <fabricio.magnus92@outlook.com>
2024-01-04 18:52:37 -0300

    add: alterações do site

--- src/components/firstBox.tsx
+++ src/components/firstBox.tsx
@@ -26,7 +26,7 @@ export default function FirstBox() {
         flexDir={{ lg: "row", md: "column-reverse", sm: "column-reverse" }}
       >
         <Flex
-          w={{ lg: "60%", md: "100%", sm: "100%" }}
+          w={{ lg: "50%", md: "100%", sm: "100%" }}
           h={{ lg: "100%", md: "40vh", sm: "40vh" }}
           p={10}
         >
@@ -47,25 +47,18 @@ export default function FirstBox() {
           </Text>
         </Flex>
         <Flex
-          w={{ lg: "40%", md: "100%", sm: "100%" }}
+          w={{ lg: "50%", md: "100%", sm: "100%" }}
           h={{ lg: "100%", md: "40vh", sm: "40vh" }}
-          bgColor={"yellow"}
+          justifyContent={"center"}
+          alignItems={"center"}
         >
-          <Text
-            color={colors.corBackground}
-            fontSize={{ lg: "xl", md: "md", sm: "sm" }}
-            my={"auto"}
-            fontFamily="Century Gothic"
-          >
-            Prazer! Eu me chamo Mariana, mas pode me chamar de Mari. Estou
-            próxima da casa dos 30, e me encontrei no ramo profissional, mais
-            especificamente na área de criação. Apesar da pouca experiência, eu
-            já fiz de tudo um pouco nessa vida. Me encontrei no ramo do design
-            como uma forma de expressar e extravasar minha criatividade.
-            Atualmente, curso produção audiovisual, e cursei alguns semestres de
-            jornalismo. Unindo duas paixões que tenho na vida profissional: a
-            arte de comunicar e ilustrar a história contada.
-          </Text>
+          <Flex
+            w={"500px"}
+            h={"500px"}
+            justifyContent={"center"}
+            bgColor={"#EF9908"}
+            borderRadius={"50%"}
+          ></Flex>
         </Flex>
       </Flex>
     </Flex>
