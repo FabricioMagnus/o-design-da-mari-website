ebe454f8e08861baa19778bc4932b103a1a0f753
Fabricio Magnus <fabricio.magnus92@outlook.com>
2024-01-04 21:14:55 -0300

    add: alterações do site

--- src/components/secondBox.tsx
+++ src/components/secondBox.tsx
@@ -1,8 +1,28 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Image, Text } from "@chakra-ui/react";
 import "./firstBox.css";
 import "../styles.css";
+import CustomSwiper from "./swipper/swipper";
 
 export default function SecondBox() {
+  const Images = [
+    {
+      src: "./certificados/edicao-video.jpg",
+      alt: "edicao-video",
+    },
+    {
+      src: "./certificados/entrando-jogo.jpg",
+      alt: "entrando-jogo",
+    },
+    {
+      src: "./certificados/wordpress.jpg",
+      alt: "wordpress",
+    },
+    {
+      src: "./certificados/copy.jpg",
+      alt: "copy",
+    },
+  ];
+
   return (
     <Flex
       w={"99.2vw"}
@@ -15,12 +35,7 @@ export default function SecondBox() {
       flexDir={"column"}
     >
       <Flex w={"100%"} h={"10%"} alignItems={"center"}>
-        <Text
-          ml={"10%"}
-          fontFamily={"FingerPaint"}
-          fontSize={"5xl"}
-          color={"black"}
-        >
+        <Text ml={"10%"} fontFamily={""} fontSize={"5xl"} color={"black"}>
           Certificados e Formações
         </Text>
       </Flex>
@@ -31,7 +46,13 @@ export default function SecondBox() {
           justifyContent={"center"}
           alignItems={"center"}
         >
-          <Flex w={"60%"} h={"90%"} bgColor={"black"}></Flex>
+          <Flex w={"60%"} h={"90%"} bgColor={"black"}>
+            <CustomSwiper>
+              {Images.map((image, index) => (
+                <Image key={index} src={image.src} alt={image.alt} />
+              ))}
+            </CustomSwiper>
+          </Flex>
         </Flex>
         <Flex w={"100%"} h={"15%"}></Flex>
       </Flex>
---
+++ src/components/swipper/swipper.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./styles.css";
+import {
+  Pagination,
+  Navigation,
+  Scrollbar,
+  A11y,
+  Mousewheel,
+} from "swiper/modules";
+import { Flex } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+interface CustomSwiperProps {
+  children: ReactNode;
+}
+
+export default function CustomSwiper({ children }: CustomSwiperProps) {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index: number, className: string) {
+      return `<span class="${className}" >${index + 1}</span>`;
+    },
+  };
+
+  return (
+    <>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={2}
+        slidesPerView={1}
+        navigation
+        pagination={pagination}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper: any) => {}}
+        onSlideChange={() => {}}
+        className="mySwiper"
+      >
+        {React.Children.map(children, (child, index) => (
+          <SwiperSlide key={index}>
+            <Flex
+              w={"100%"}
+              h={"100%"}
+              bgColor={"#D32982"}
+              justifyContent={"center"}
+            >
+              {child}
+            </Flex>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
